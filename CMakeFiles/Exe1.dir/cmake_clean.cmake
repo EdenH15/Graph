27ac716eeac99cbe821b497aff06dda476c01c83
edenh15 <edenhassin@gmail.com>
2025-03-24 16:00:05 +0200

    new

---
+++ CMakeFiles/Exe1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exe1.dir/graph.cpp.o"
+  "CMakeFiles/Exe1.dir/graph.cpp.o.d"
+  "CMakeFiles/Exe1.dir/main.cpp.o"
+  "CMakeFiles/Exe1.dir/main.cpp.o.d"
+  "Exe1"
+  "Exe1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exe1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ graph.h
@@ -0,0 +1,32 @@
+//
+// Created by edenh15 on 3/24/25.
+//
+
+#ifndef GRAPH_H
+#define GRAPH_H
+namespace graph {
+
+    struct Neighbor {
+        int vertex;
+        int weight;
+        Neighbor* next;
+    };
+
+    class Graph {
+    private:
+        int numV;
+        Neighbor** adjacencyList;
+    public:
+        Graph(int vertices);
+        void addEdge(int src, int dst, int weight=1);
+        bool edgeExists(int src, int dst);
+        void removeEdge(int src, int dst);
+        void removeEdgeOneWay(int src, int dst);
+        void print_graph();
+        void addNeighbor(int src, int dst, int weight);
+        void releaseMemory();
+    };
+}
+
+#endif
+
